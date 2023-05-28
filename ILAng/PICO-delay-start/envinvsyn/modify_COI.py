1d1b1057b968a7858236cf4e5098b1201dc7f50b
Hongce Zhang <zhanghongce@126.com>
2023-05-28 17:51:59 +0800

    update pico-delay-start

---
+++ ILAng/PICO-delay-start/envinvsyn/modify_COI.py
@@ -0,0 +1,15 @@
+ret = ''
+
+modify_info={'RTL.dbg_insn_addr':['1', '1','0']}
+
+with open('COI.txt') as fin:
+    for line in fin.readlines():
+        line = line.split()
+        if line[0] in modify_info:
+          slices = modify_info[line[0]]
+          line = [line[0]] + slices
+        ret += ' '.join(line) + '\n'
+
+with open('COI.txt','w') as fout:
+    fout.write(ret)
+    
